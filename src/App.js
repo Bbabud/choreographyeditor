46140b9ecd366bb194948679c6968f37e9c7c8e7
Bbabud <56270250+Bbabud@users.noreply.github.com>
2019-12-02 03:57:42 +0100

    forTravisCi

--- src/App.js
+++ src/App.js
@@ -3,36 +3,12 @@ import Menu from "./components/navbar";
 import LayOut from "./components/layout";
 import "./css/app.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Step from "./components/steps";
-
 class App extends Component {
   render() {
-    const waltzSteps = [
-      new Step(
-        1,
-        "Basic Forward Step",
-        0,
-        0,
-        0,
-        1,
-        1,
-        require("./assets/BasicForwardStep.png")
-      ),
-      new Step(
-        2,
-        "Basic Backward Step",
-        0,
-        0,
-        0,
-        -1,
-        -1,
-        require("./assets/BasicBackwardStep.png")
-      )
-    ];
     return (
       <React.Fragment>
         <Menu />
-        <LayOut waltzSteps={waltzSteps} />
+        <LayOut />
       </React.Fragment>
     );
   }
