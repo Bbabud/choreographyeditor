49cc96b0a69c50b836e5eacf1f4938cb01b4b81b
Bbabud <56270250+Bbabud@users.noreply.github.com>
2019-12-02 04:00:39 +0100

    files

---
+++ src/files/images.js
@@ -0,0 +1,13 @@
+import BFS from "../assets/BasicForwardStep.png";
+import BBS from "../assets/BasicBackwardStep.png";
+
+const Images = {
+  adata: {
+    Waltz: [
+      { name: "Basic Forward Step", image: BFS },
+      { name: "Basic Backward Step", image: BBS }
+    ]
+  }
+};
+
+export default Images;
